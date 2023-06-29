aa9e493d2c17693010fd6e1aa71c8721996f568c
Swaroop Nambiar <swaroopnambiar04@gmail.com>
2023-06-29 12:43:12 +0400

    Third commit

--- Glossary maker.py
+++ Glossary maker.py
@@ -1,8 +1,7 @@
-f=open("paragraph.txt","wt")
-List=["The purpose of a paragraph is to express a speakers thoughts on a particular point in a clear way that is unique and specific to that paragraph . In other words  , paragraphs shouldn't be mixing thoughts or idea . When a new idea is introduce , generally , a writer will introduce a new paragraph . Paragraphs provide structure and flow to your text . And random paragraph allow you to move from one thought to another . When you start a new paragraph you are telling your reader that the topic is over and you are moving on . Without this structure , your brilliant ideas and your sound argumentation will be difficult to follow . One of the best way to get started with your paragraph phrasing routine is to get a relevant tool to try your skills on . What else can be good than wordigram itself? Here's a small overview of this amazing platform for sentences , words , nouns , adjectives , hashtags and paragraph generation . "]
-f.writelines(List)
-f.close()
-
+# f=open("paragraph.txt","wt")
+# List=["The purpose of a paragraph is to express a speakers thoughts on a particular point in a clear way that is unique and specific to that paragraph . In other words  , paragraphs shouldn't be mixing thoughts or idea . When a new idea is introduce , generally , a writer will introduce a new paragraph . Paragraphs provide structure and flow to your text . And random paragraph allow you to move from one thought to another . When you start a new paragraph you are telling your reader that the topic is over and you are moving on . Without this structure , your brilliant ideas and your sound argumentation will be difficult to follow . One of the best way to get started with your paragraph phrasing routine is to get a relevant tool to try your skills on . What else can be good than wordigram itself? Here's a small overview of this amazing platform for sentences , words , nouns , adjectives , hashtags and paragraph generation . "]
+# f.writelines(List)
+# f.close()
 
 f1=open("paragraph.txt","rt")      
 lines=f1.read()
@@ -22,18 +21,16 @@ for ch in Allwords:
       else:
         glossary.append(ch)
 
-glossary.sort()       
-print(glossary)
-print()
-print()
-print()
-print ('\t\t\t\t',"GLOSSARY")
-print()
-print()
+glossary.sort()
 
-p=0
-while p<len(glossary)-1:
-  
-  print('\t',"%-15s %-15s %-15s %s" %(glossary[p],glossary[p+1],glossary[p+2],glossary[p+3]))
-  print()
-  p=p+4
+print("\n\n\n\t\t\t\tGLOSSARY\n\n")
+
+p = 0
+while p < len(glossary):
+    if p + 3 < len(glossary):
+        print("\t%-15s %-15s %-15s %s" % (glossary[p], glossary[p + 1], glossary[p + 2], glossary[p + 3]))
+        p += 4
+    else:
+        print("\t%-15s" % glossary[p])
+        p += 1
+    print()
